e6e252b9274dae9318a2d045d0991e8e7cf2ff66
Oleksandr Koval <oleksandrkoval@Oleksandrs-MacBook-Pro.local>
2021-07-06 17:50:21 +0300

    Refactor code

--- src/index.js
+++ src/index.js
@@ -1,25 +1,11 @@
+exports.min = function min (array = []) {
+  return array.length !== 0 ? Math.min(...array) : 0;
+} 
 
-exports.min = function min (array) {
-  if (array == undefined || array.length == 0) {
-    return 0;
-  } else {
-    return array.sort( (a, b) => a - b )[0];
-  }
-
+exports.max = function max (array = []) {
+  return array.length !== 0 ? Math.max(...array) : 0;
 }
 
-exports.max = function max (array) {
-  if (array == undefined || array.length == 0) {
-    return 0;
-  } else {
-    return array.sort( (a, b) => a - b )[array.length - 1];
-  }
-}
-
-exports.avg = function avg (array) {
-  if (array == undefined || array.length == 0) {
-    return 0;
-  } else {
-    return array.reduce((sum, current) => sum + current, 0)/array.length;
-  }
+exports.avg = function avg (array = []) {
+  return array.length !== 0 ? array.reduce((a, b) => a + b)/array.length : 0;
 }
